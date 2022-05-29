afd02e8ccae34889eb3976615f069a2850ec485d
Swartvaghe <floo.swartzy@hotmail.fr>
2022-05-30 00:30:05 +0200

    Récupérer les infos de tout le monde ou une personne sans le(s) mot(s) de passe

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
+
+// auth
 router.post('/register', authController.signUp);
+
+// user
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getOneUser);
 
 module.exports = router
