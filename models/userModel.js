556af52c56529ed60491d0e6e48868ac5a97cc1b
Swartvaghe <floo.swartzy@hotmail.fr>
2022-05-30 00:00:30 +0200

    Creation d'utilisateur

---
+++ models/userModel.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+    {
+        lastName: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 55,
+            trimp: true
+        },
+        firstName: {
+            type: String,
+            required: true,
+            minlength: 3,
+            maxlength: 55,
+            trimp: true
+        },
+        email: {
+            type: String,
+            required: true,
+            validate: [isEmail],
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            require: true,
+            max: 1024,
+            minlength: 6
+        },
+        picture: {
+            type: String,
+            default: './uploads/profil/random-user.png'
+        },
+        likes: {
+            type: [String]
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const UserModel = mongoose.model('user', userSchema);
+
+module.exports = UserModel;
