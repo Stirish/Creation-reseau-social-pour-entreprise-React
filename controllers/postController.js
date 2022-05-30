367bd8ecf5bd8bf2a9a19ff8561a1e594feaeaa4
Swartvaghe <floo.swartzy@hotmail.fr>
2022-05-30 04:09:17 +0200

    Update des posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,7 +26,22 @@ module.exports.getAllPost = (req, res) => {
 };
 
 module.exports.updatePost = (req, res) => {
-    
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id);
+
+    const updatedRecord = {
+        message: req.body.message
+    };
+
+    PostModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updatedRecord },
+        { new: true },
+        (err, docs) => {
+            if (!err) res.send(docs)
+            else console.log('Update error : ' + err);
+        }
+    )
 };
 
 module.exports.deletePost = (req, res) => {
