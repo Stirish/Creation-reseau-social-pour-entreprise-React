654267d5e22b590699db8c93987c6c6f14bfcf58
Swartvaghe <floo.swartzy@hotmail.fr>
2022-05-30 04:13:28 +0200

    Suppression des posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -45,5 +45,14 @@ module.exports.updatePost = (req, res) => {
 };
 
 module.exports.deletePost = (req, res) => {
+    if(!ObjectID.isValid(req.params.id))
+        return res.status(400).send('ID unknown : ' + req.params.id);
     
+    PostModel.findByIdAndRemove(
+        req.params.id,
+        (err, docs) => {
+            if (!err) res.send(docs);
+            else console.log('Delete error : ' + err);
+        }
+    )
 };
