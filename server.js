a5cff7c106ca455d2c979c5ed04c226f0b6362dd
Swartvaghe <floo.swartzy@hotmail.fr>
2022-05-30 02:27:23 +0200

    Creation de middleware pour les verifications des tokens

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes')
+const cookieParser = require('cookie-parser');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
+const { checkUser, requireAuth } = require('./middleware/auth');
 const app = express();
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+// jwt
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+});
+
 
 // routes
 app.use('/api/user', userRoutes);
