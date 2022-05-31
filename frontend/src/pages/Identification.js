d60c3c31a0d8a7be44d453667c6939e2e2e70878
Swartvaghe <floo.swartzy@hotmail.fr>
2022-05-31 23:09:56 +0200

    Verification du token côté frontend

--- frontend/src/pages/Identification.js
+++ frontend/src/pages/Identification.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Log from '../components/log'
+import { UidContext } from "../components/AppContext";
 
 const Identification = () => {
-    return(
+    const uid = useContext(UidContext);
+
+    return (
         <div className='profil-page'>
+            { uid ? (
+            <h1>Deconnexion</h1> 
+            ) : ( 
             <div className='log-container'>
                 <Log signin={false} signup={true} />
                 <div className='img-container'>
                     <img src='./img/logos/icon-left-font.svg' alt='logo groupomania' />
                 </div>
             </div>
+            )}
         </div>
     );
 };
