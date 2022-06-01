5fd648c648bd46e92027b16baa9ddf4c2e5e8c32
Swartvaghe <floo.swartzy@hotmail.fr>
2022-06-01 10:02:33 +0200

    infinit scroll

--- frontend/src/actions/postActions.js
+++ frontend/src/actions/postActions.js
@@ -5,12 +5,13 @@ export const LIKE_POST = 'LIKE_POSTS';
 export const UNLIKE_POST = 'UNLIKE_POST';
 
 
-export const getPosts = () => {
+export const getPosts = (num) => {
     return (dispatch) => {
         return axios
             .get(`${process.env.REACT_APP_API_URL}api/post/`)
             .then((res) => {
-                dispatch({ type: GET_POSTS, payload: res.data })
+                const array = res.data.slice(0, num)
+                dispatch({ type: GET_POSTS, payload: array })
             })
             .catch((err) => console.log(err))
     };
